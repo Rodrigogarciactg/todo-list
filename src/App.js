c193d2c35849527e8fa59868c2a9b2351bf6fa5f
Rodrigogarciactg <100664773+Rodrigogarciactg@users.noreply.github.com>
2022-04-24 16:24:08 -0700

    Todo List MERN PT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  // useState always returns an array with two items in it
+  // then create two variables to store the first & second item of the array
+  // ("") = starting value of the state
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleNewTodoSubmit = (event) => {
+    event.preventDefault();
+    if (newTodo.length === 0) {
+      return;
+    }
+
+    const todoItem = {
+      text: newTodo,
+      complete: false,
+    };
+
+    // setTodos and pass in a brand new array containing all current todos + 1 more
+    setTodos([...todos, todoItem]);
+    setNewTodo("");
+  };
+
+  const handleTodoDelete = (delIdx) => {
+    const filteredTodos = todos.filter((todo, i) => {
+      return i != delIdx;
+    });
+    setTodos(filteredTodos);
+  };
+
+  const handleToggleComplete = (idx) => {
+    const updatedTodos = todos.map((todo, i) => {
+      if (idx === i) {
+        todo.complete = !todo.complete;
+        // To avoid mutating the todo directly, do this:
+        // const updatedTodo = { ...todo, complete: !todo.complete };
+        // return updatedTodo;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}>
+      <form
+        onSubmit={(event) => {
+          handleNewTodoSubmit(event);
+        }}
+      >
+        <input
+          onChange={(event) => {
+            setNewTodo(event.target.value);
+          }}
+          type="text"
+          value={newTodo}
+        />
+        <div>
+          <button>Add</button>
+        </div>
+      </form>
+      <hr />
+      {todos.map((todo, i) => {
+        const todoClasses = ["bold", "italics"];
+
+        if (todo.complete) {
+          todoClasses.push("line");
+        }
+
+        return (
+          <div key={i}>
+            <input
+              onChange={(event) => {
+                handleToggleComplete(i);
+              }}
+              checked={todo.complete}
+              type="checkbox"
+            />
+            <span className={todoClasses.join(" ")}>{todo.text}</span>
+            <button
+              onClick={(event) => {
+                handleTodoDelete(i);
+              }}
+              style={{ marginLeft: "10px" }}
+            >
+              Delete
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
